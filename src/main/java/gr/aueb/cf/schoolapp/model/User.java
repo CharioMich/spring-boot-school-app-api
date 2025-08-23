5f2024709584ed4aea8b9a12b5d0a166caa22319
Ch. Michalakis <mpampisb@gmail.com>
2025-08-23 15:39:57 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/schoolapp/model/User.java
@@ -0,0 +1,109 @@
+package gr.aueb.cf.schoolapp.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import gr.aueb.cf.schoolapp.core.enums.GenderType;
+import gr.aueb.cf.schoolapp.core.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class User extends AbstractEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(unique = true)
+    private String afm;
+
+    @Column(name = "father_name")
+    private String fatherName;
+
+    @Column(name = "mother_name")
+    private String motherName;
+
+    @Column(name = "father_lastname")
+    private String fatherLastname;
+
+    @Column(name = "mother_lastname")
+    private String motherLastname;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    private GenderType gender;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @ColumnDefault("true")          // Default value == true
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @OneToOne(mappedBy = "user")    // Bi-direction
+    private Teacher teacher;
+
+    @OneToOne(mappedBy = "user")    // Bi-direction
+    private Employee employee;
+
+    public Boolean isTeacher() {
+        return getTeacher() != null;    // Lombok Getter
+    }
+
+    public Boolean isEmployee() {
+        return getEmployee() != null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
---
+++ src/main/java/gr/aueb/cf/schoolapp/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.schoolapp.repository;
+
+import gr.aueb.cf.schoolapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+}
