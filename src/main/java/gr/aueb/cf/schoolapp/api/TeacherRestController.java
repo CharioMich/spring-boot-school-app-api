23d74e0e40f5625cd360b7834b091e6386b9e6ff
Ch. Michalakis <mpampisb@gmail.com>
2025-09-01 13:56:36 +0300

    refactor: add openapi documentation and small changes

--- src/main/java/gr/aueb/cf/schoolapp/api/TeacherRestController.java
+++ src/main/java/gr/aueb/cf/schoolapp/api/TeacherRestController.java
@@ -6,10 +6,18 @@ import gr.aueb.cf.schoolapp.core.filters.TeacherFilters;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolapp.service.TeacherService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.validation.BindingResult;
@@ -17,29 +25,72 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.security.Principal;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@RequiredArgsConstructor    // for DI
+@RequiredArgsConstructor
 public class TeacherRestController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TeacherRestController.class);
     private final TeacherService teacherService;
 
 
-    /**
-     * REST Controller. Saves a new Teacher entity
-     * @param teacherInsertDTO @Valid -> Enables Hibernate-JPA validation as per the annotated DTOs (Records).
-     * @param amkaFile @RequestPart because we have the DTO and the file. So two parts.
-     * @param bindingResult Error container if errors occur
-     * @return ResponseEntity container, handles the data and the Response Status code
-     */
+    @Operation(
+            summary = "Get all teachers paginated",
+            security = @SecurityRequirement(name = "Bearer Authentication"),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Teachers Found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TeacherReadOnlyDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Access Denied",
+                            content = @Content
+                    )
+            }
+    )
+    @GetMapping("/teachers")
+    public ResponseEntity<Page<TeacherReadOnlyDTO>> getPaginatedTeachers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "1") int size) {
+
+        Page<TeacherReadOnlyDTO> teachersPage = teacherService.getPaginatedTeachers(page, size);
+        return new ResponseEntity<>(teachersPage, HttpStatus.OK);
+    }
+
+
+
+    @Operation(
+            summary = "Save a teacher",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Teacher inserted",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TeacherReadOnlyDTO.class)
+                            )
+                    )
+            }
+    )
     @PostMapping("/teachers/save")
     public ResponseEntity<TeacherReadOnlyDTO> saveTeacher(
             @Valid @RequestPart(name = "teacher")TeacherInsertDTO teacherInsertDTO,
-            @Nullable @RequestPart("amkaFile") MultipartFile amkaFile,
+            @RequestPart("amkaFile") MultipartFile amkaFile,
             BindingResult bindingResult)
-            throws AppObjectInvalidArgumentException, ValidationException, AppObjectAlreadyExists, AppServerException, IOException {
+            throws AppObjectInvalidArgumentException, ValidationException, AppObjectAlreadyExists, AppServerException {
 
         if (bindingResult.hasErrors()) {
             throw new ValidationException(bindingResult);
@@ -47,47 +98,88 @@ public class TeacherRestController {
 
         try {
             TeacherReadOnlyDTO teacherReadOnlyDTO = teacherService.saveTeacher(teacherInsertDTO, amkaFile);
-            return new ResponseEntity<>(teacherReadOnlyDTO, HttpStatus.CREATED);
+
+            return new ResponseEntity<>(teacherReadOnlyDTO, HttpStatus.OK);
         } catch (IOException e) {
-            throw new AppServerException("Attachment", "Attachment cannot get uploaded"); // Catch the IOException and throw custom AppServerException to be handled by our custom global Exception Handler
+            throw new AppServerException("Attachment", "Attachment can not get uploaded");
         }
     }
 
 
-    /**
-     * REST Controller GET paginated teachers
-     * @param page with @RequestParam, we give sa default value. Note that it's a string
-     * @param size of elements per page
-     * @return ResponseEntity container > Page > TeacherReadOnlyDTO. We could also return custom Paginated container instead of Page
-     */
-    @GetMapping("/teachers/paginated")
-    public ResponseEntity<Page<TeacherReadOnlyDTO>> getPaginatedTeachers(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "5") int size) {
 
-        Page<TeacherReadOnlyDTO> teachersPage = teacherService.getPaginatedTeachers(page, size);
-        return new ResponseEntity<>(teachersPage, HttpStatus.OK);
+    @Operation(
+            summary = "Get all teachers filtered",
+            security = @SecurityRequirement(name = "Bearer Authentication"),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Teachers Found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TeacherReadOnlyDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Access Denied",
+                            content = @Content
+                    )
+            }
+    )
+    @PostMapping("/teachers/all")
+    public ResponseEntity<List<TeacherReadOnlyDTO>> getTeachers(@Nullable @RequestBody TeacherFilters filters,
+                                                                Principal principal)
+            throws AppObjectNotFoundException, AppObjectNotAuthorizedException {
+        try {
+            if (filters == null) filters = TeacherFilters.builder().build();
+            return ResponseEntity.ok(teacherService.getTeachersFiltered(filters));
+        } catch (Exception e) {
+            LOGGER.warn("Could not get teachers.", e);
+            throw e;
+        }
     }
 
 
-    /**
-     * @param filters @RequestBody -> JSON. @Nullable -> May be null. We may have or not have filters
-     */
-    @PostMapping("/teachers/filtered")
-    public ResponseEntity<List<TeacherReadOnlyDTO>> getFilteredTeachers(@Nullable @RequestBody TeacherFilters filters)
-        throws AppObjectNotAuthorizedException {
-        if (filters == null) TeacherFilters.builder().build();  // Create an instance but without any filters. Return ALL
-        return ResponseEntity.ok(teacherService.getTeachersFiltered(filters));
-    }
 
-
-    /**
-     * @return Teachers filtered and Paginated via custom Paginated container wrapper by the ResponseEntity
-     */
-    @PostMapping("/teachers/filtered/paginated")
-    public ResponseEntity<Paginated<TeacherReadOnlyDTO>> getTeachersFilteredPaginated(@Nullable @RequestBody TeacherFilters filters)
-        throws AppObjectNotAuthorizedException {
-        if (filters == null) TeacherFilters.builder().build();  // Create an instance but without any filters. Return ALL
-        return ResponseEntity.ok(teacherService.getTeachersFilteredPaginated(filters));
+    @Operation(
+            summary = "Get all teachers filtered",
+            security = @SecurityRequirement(name = "Bearer Authentication"),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Teachers Found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TeacherReadOnlyDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Access Denied",
+                            content = @Content
+                    )
+            }
+    )
+    @PostMapping("/teachers/all/paginated")
+    public ResponseEntity<Paginated<TeacherReadOnlyDTO>> getTeachersFilteredPaginated(@Nullable @RequestBody TeacherFilters filters,
+                                                                                      Principal principal)
+            throws AppObjectNotFoundException, AppObjectNotAuthorizedException {
+        try {
+            if (filters == null) filters = TeacherFilters.builder().build();
+            return ResponseEntity.ok(teacherService.getTeachersFilteredPaginated(filters));
+        } catch (Exception e) {
+            LOGGER.warn("Could not get teachers.", e);
+            throw e;
+        }
     }
 }
